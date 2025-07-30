62d1f98bbc7b2a8ea5c456c58859f4d531487b54
Ilham Wahdini <ilhamwahdini@Ilhams-MacBook.local>
2025-07-30 16:06:52 +0800

    first commit

---
+++ DAG.py
@@ -0,0 +1,104 @@
+'''
+=================================================
+Milestone 3
+
+Nama  : Ilham Wahdini
+Batch : RMT-043
+
+This program is to do automation tranform and load data from PostgreSQL to ElasticSearch. Dataset used is data of Global Renewable Energy Usage.
+=================================================
+'''
+
+# Import Libraries
+
+import datetime as dt
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import pandas as pd
+import psycopg2 as db
+from elasticsearch import Elasticsearch
+
+# Functions 
+
+# Fetch Data from PostgreSQL
+def fetch_from_postgres():
+    '''
+    This function is to load or fetch data from PostgreSQL and save the data to csv file as raw data.
+    '''
+    conn_string = "dbname='airflow' host='postgres' user='airflow' password='airflow' port='5432'" #access to PostgreSQL
+    conn = db.connect(conn_string)
+
+    query = "SELECT * FROM table_m3"
+    df = pd.read_sql(query, conn)
+    df.to_csv('/opt/airflow/dags/P2M3_ilham_wahdini_data_raw.csv', index=False) #save as csv file
+
+    conn.close() #to close connection
+
+# Data Cleaning
+def data_cleaning():
+    '''
+    This function is to clean the dataset and then save as clean data. There are two step of data cleaning, drop duplicates and handle missing value.
+    '''
+    df = pd.read_csv('/opt/airflow/dags/P2M3_ilham_wahdini_data_raw.csv') #load data
+
+    # 1. Drop duplicates
+    df.drop_duplicates(inplace=True) #drop duplicated data, and make every data unique
+
+    # 2. Handle missing values
+    # - Categorical = 'unknown', assume str unknown as category that not in in the class.
+    # - Numerical = 0, all the numerical value in dataset are not/cannot be zero, assume zero is value of missing value.
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col].fillna('unknown', inplace=True)
+        else:
+            df[col].fillna(0, inplace=True)
+
+    # Save cleaned data
+    df.to_csv('/opt/airflow/dags/P2M3_ilham_wahdini_data_clean.csv', index=False) #save clean data as csv file
+
+# Add to ElasticSearch
+def to_elasticsearch():
+    '''
+    This function is to change data to json and add the data to ElasticSearch.
+    '''
+    es = Elasticsearch("http://elasticsearch:9200")
+    df = pd.read_csv('/opt/airflow/dags/P2M3_ilham_wahdini_data_clean.csv') #load clean data
+
+    for i,r in df.iterrows():
+        doc=r.to_json()
+        es.index(index="projectm3", doc_type="doc", body=doc, id=i)
+
+# DAG Setup
+
+default_args = {
+    'owner': 'ilham',
+    'start_date': dt.datetime(2024, 11, 1, 9, 10, 0) - timedelta(hours=8), #run time strat at 01 November 2024 at 9.10am
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=1),
+}
+
+with DAG('PROJECTDAG',
+         default_args=default_args,
+         schedule_interval= '10-30/10 9 * * 6', #the run will be every saturday every 10min from 9.10am until after 9.30am(last run on that day)
+         catchup=False #skip the run between start date and present run
+         ) as dag:
+
+    # Tasks (call all functions)
+    
+    task_fetch = PythonOperator(
+        task_id='fetch_from_postgres',
+        python_callable=fetch_from_postgres,
+    )
+
+    task_clean = PythonOperator(
+        task_id='data_cleaning',
+        python_callable=data_cleaning,
+    )
+
+    task_es = PythonOperator(
+        task_id='to_elasticsearch',
+        python_callable=to_elasticsearch,
+    )
+
+task_fetch >> task_clean >> task_es #run sequence
---
+++ README.md
@@ -0,0 +1,52 @@
+# Data Analysis and Processing of Global Renewable Energy Usage FY 2020-2024
+
+## Repository Outline
+1. README.md - About the project
+2. DAG.py - Code script for DAG (Directed Acyclic Graph) with task of data processing
+3. GX.ipynb - Code script for Great Expectation analysis
+5. data_raw.csv - Raw dataset from source
+6. data_clean.csv - Data set after cleaning
+7. /image - folder contains images of vizulaization using Kibana
+
+
+## Problem Background
+The global shift towards sustainable energy sources has prompted governments and organizations to closely monitor the adoption and usage patterns of renewable energy. However, dataset is often fragmented, inconsistently formatted, and stored in different systems. To support data driven policy making, there is a growing need for automated data pipelines that can extract, clean, and visualize renewable energy usage data and so the analysis and give recommendation. This project aims to build a pipeline using Apache Airflow. Data is fetched from a PostgreSQL database, cleaned and standardized to ensure quality, and then insert into Elasticsearch for visualization with Kibana. Through dashboards, can gain insights into the effectiveness of planning and evaluation of renewable energy programs.
+
+## Project Output
+- DAG (Directed Acyclic Graph) built with Apache Airflow.
+- Cleaned and validated dataset.
+- Vizualization and analysis (recommendation) from data
+
+## Data
+The dataset used in this project was sourced from Kaggle.com. Dataset contains information on renewable energy adoption and usage by households around the world for year 2020-2024. It provides insights into the growing trend of renewable energy. The dataset format in csv nad has 1000 datas and 12 columns. List of columns:
+1. Household_ID: A unique identifier for each household.
+2. Region: The geographical region where the household is located
+3. Country: The specific country of the household.
+4. Energy_Source: The type of renewable energy being used by the household
+5. Monthly_Usage_kWh: The monthly energy consumption in kilowatt-hours.
+6. Year: The year the data was recorded (2020-2024).
+7. Household_Size: The number of people living in the household.
+8. Income_Level: The income bracket of the household (Low, Middle, High).
+9. Urban_Rural: Whether the household is in an urban or rural area.
+10. Adoption_Year: The year the household first adopted renewable energy.
+11. Subsidy_Received: Whether the household received any government subsidies (Yes/No).
+12. Cost_Savings_USD: The monthly savings in USD due to using renewable energy.
+
+## Method
+This project employs Python and its libararies to connect with PostgreSQL, conduct DAG for Apache Airflow and transfer data to ElasticSearch. Using KIbana for data visualization. Python also used for analysis Great Expectation.
+
+## Stacks
+The programming language, tools, and Python libraries utilized in this project, including:
+Pandas – used for data manipulation and analysis.
+Psycopg2 - used to connect with PostgreSQL to fetch the data feom database.
+Airflow - used to create DAG and connect with Apache Airflow platform.
+Elasticsearch - used to tranfer data to ElasticSearch, so can use data with Kibana.
+Great Expectations - used for doing data validation and meet expectation.
+
+## Reference
+- [Dataset Source](https://www.kaggle.com/datasets/hajraamir21/global-renewable-energy-usage-2020-2024)
+
+---
+
+**Other Reference:**
+- 
